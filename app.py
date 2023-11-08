8b5c6765448f710f95b1a33d3e3085f02a2ac9db
pannoniae <pannoniae@users.noreply.github.com>
2023-11-08 20:54:48 +0000

    - fix ips not being shown

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask
 from flask import Flask, request
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 
@@ -15,10 +16,15 @@ def post_comment():
     comment = request.args.get('comment')
     if author == "" or comment == "":
         return "Author or comment not filled out"
+    if author is None or comment is None:
+        return "Author or comment not filled out"
     with open("comments.txt", "a") as f:
         f.write(f"{author}: {comment}\n")
     return flask.redirect("/")
 
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001)
